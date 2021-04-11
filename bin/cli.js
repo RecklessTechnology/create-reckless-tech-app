4a8b764adeb9faee72db9854b6b5df67bf55cf6b
Travis Bennett <travis@travisbennett.com>
2021-04-11 13:50:53 -0700

    New build process.

---
+++ bin/cli.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+import { execSync } from 'child_process';
+
+const repoName = process.argv[2];
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/RecklessTechnology/create-reckless-tech-app ${repoName}`;
+const installCRACommand = `cd ${repoName} && nxp create-react-app ${repoName}`;
+const installDepsCommand = `cd ${repoName} && npm install`;
+
+// Executes a command in bash
+const runCommand = command => {
+  try { // try to execute command
+    execSync(`${command}`, {stdio: 'inherit'});
+  } catch (e) { // command failed
+    console.error(`Failed to execute ${command}`, e);
+    return false;
+  }
+  return true; // command succeeded
+}
+
+// Checkout repo
+console.log(`Cloning as ${repoName}...`);
+const checkedOut = runCommand(gitCheckoutCommand);
+if (!checkedOut) process.exit(code: -1);
+
+// Install create-react-app
+console.log(`Starting with create-react-app...`);
+const installCRA = runCommand(installCRACommand);
+if (!installCRA) process.exit(code: -1);
+
+// Install dependencies
+console.log(`Installing dependencies for ${repoName}...`);
+const installDeps = runCommand(installDepsCommand);
+if (!installDeps) process.exit(code: -1);
+
+// Install complete
+console.log(`Installation ready. Use the following command to start.`);
+console.log(`cd ${repoName} && npm start`);
--- index.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-'use strict';
-const args = process.argv.slice(2);
-console.log("Hello, World", args);
