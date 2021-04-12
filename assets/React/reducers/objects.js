6018362f77c2f3ace371793336f6d0527e220d02
Travis Bennett <travis@travisbennett.com>
2021-04-11 17:29:52 -0700

    let's try this again.

--- assets/React/reducers/objects.js
+++ assets/React/reducers/objects.js
@@ -31,7 +31,7 @@ const initialState = {
       responsiveness: 20,
     },
     {
-      url: '/Models/cube/scene.gltf',
+      url: '/models/cube/scene.gltf',
       active: false,
       hover: false,
       id: 'GLTF Cube',
@@ -41,7 +41,7 @@ const initialState = {
       responsiveness: 20,
     },
     {
-      url: '/Models/cone/scene.gltf',
+      url: '/models/cone/scene.gltf',
       active: false,
       hover: false,
       id: 'GLTF Cone',
