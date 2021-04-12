10194f7381c693a0537e6111d9af23d88d459879
Travis Bennett <travis@travisbennett.com>
2021-04-11 17:08:51 -0700

    Add models.

--- bin/cli.js
+++ bin/cli.js
@@ -6,8 +6,9 @@ const repoName = process.argv[2];
 const gitCheckoutCommand = `git clone --depth 1 https://github.com/RecklessTechnology/create-reckless-tech-app ${repoName}`;
 const installCRACommand = `cd ${repoName}/ && npx create-react-app ${repoName}`;
 const removeReactFilesCommand = `cd ./${repoName}/${repoName}/src/ && rm App.css && rm App.js && rm Index.css && rm Index.js && rm logo.svg`;
-const copyReactFilesCommand = `cd ./${repoName}/assets/React && cp -r ./* ./../../${repoName}/src/`;
-const copyDockerFilesCommand = `cd ./${repoName}/assets/Docker && cp -r ./Dockerfile ./../../${repoName}/`;
+const moveReactFilesCommand = `cd ./${repoName}/assets/React && mv ./* ./../../${repoName}/src/`;
+const moveModelFilesCommand = `cd ./${repoName}/assets/ && mv ./Models/ ./../../${repoName}/public/`;
+const moveDockerFilesCommand = `cd ./${repoName}/assets/Docker && mv ./Dockerfile ./../../${repoName}/`;
 const removeCRTAFilesCommand = `cd ./${repoName}/ && rm package.json && rm README.md && rm .gitignore`;
 const moveCRAFilesCommand = `cd ./${repoName}/${repoName}/ && mv ./* ./../`;
 const installDepsCommand = `cd ./${repoName}/ && npm install && npm install @material-ui/core @material-ui/icons @material-ui/styles @react-spring/three react-redux @react-three/fiber react-use-gesture redux redux-thunk three --save`;
@@ -41,20 +42,20 @@ if (!removeReactFiles) process.exitCode = -1;
 
 // Replace with files from create-reckless-tech-app
 console.log(`Adding CRTA files...`);
-const copyReactFiles = runCommand(copyReactFilesCommand);
-if (!copyReactFiles) process.exitCode = -1;
+const moveReactFiles = runCommand(moveReactFilesCommand);
+if (!moveReactFiles) process.exitCode = -1;
 
 // Add docker files
 console.log(`Adding Docker files...`);
-const copyDockerFiles = runCommand(copyDockerFilesCommand);
-if (!copyDockerFiles) process.exitCode = -1;
+const moveDockerFiles = runCommand(moveDockerFilesCommand);
+if (!moveDockerFiles) process.exitCode = -1;
 
 // Remove CRTA files
 console.log(`Removing CRTA files...`);
 const removeCRTAFiles = runCommand(removeCRTAFilesCommand);
 if (!removeCRTAFiles) process.exitCode = -1;
 
-// Copy CRTA files to root
+// Move CRTA files to root
 console.log(`Move CRTA files to root...`);
 const moveCRAFiles = runCommand(moveCRAFilesCommand);
 if (!moveCRAFilesCommand) process.exitCode = -1;
