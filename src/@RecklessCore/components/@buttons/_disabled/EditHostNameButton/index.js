dd8fb8c2f2bd80909da01a9a511a1ee4565b41ac
Travis Bennett <travis@travisbennett.com>
2021-10-05 11:05:48 -0700

    Remove unused files.

--- src/@RecklessCore/components/@buttons/_disabled/EditHostNameButton/index.js
+++
@@ -1,7 +0,0 @@
-import EditPeerNameButtonView from './view';
-
-const EditPeerNameButton = ({ peerInfo }) => <EditPeerNameButtonView {...{ peerInfo }} />;
-
-EditPeerNameButton.whyDidYouRender = true;
-
-export default EditPeerNameButton;
